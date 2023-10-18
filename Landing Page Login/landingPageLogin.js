18348f94856da4603c77d31f95e460250d58ca32
LuthDev9901 <luthfidwisyahputra9901@gmail.com>
2023-10-19 01:38:11 +0700

    final v2

--- Landing Page Login/landingPageLogin.js
+++ Landing Page Login/landingPageLogin.js
@@ -6,13 +6,14 @@
 //     window.location.href = '../html/meEdukasi-baca.html';
 // }
 
-function loginWarning() {
-    alert('Anda Harus Login Terlebih Dahulu!!!');
-}
-
-// function logoutWarning() {
-//     alert('Anda Akan LogOut. Apakah Anda Yakin?');
+// function loginWarning() {
+//     alert('Anda Harus Login Terlebih Dahulu!!!');
 // }
+
+function logoutWarning() {
+    alert('Anda Akan LogOut. Apakah Anda Yakin?');
+    window.location.href = '../landingPage.html';
+}
 
 // onclick = "window.location.href = './html/meEdukasi-baca.html'"
 
